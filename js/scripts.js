fe544a589557c88133653b7880325884452adee4
Esti Shay and Paul Guevarra <esti@>
2017-07-17 09:31:45 -0700

    add form that returns values onscreen

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 //business logic
+function Location(placeName, landmark, timeOfYear, accomodations, activities, notes) {
+  this.placeName = placeName;
+  this.landmark = landmark;
+  this.timeOfYear = timeOfYear;
+  this.accomodations = accomodations;
+  this.activities = activities;
+  this.notes = notes;
+}
 
+Location.prototype.displayLocation = function () {
+  return this.placeName + ": " + this.landmark + ", "  + this.timeOfYear + ", " + this.accomodations + ", " + this.activities + ", " + this.notes;
+}
 
 //UI logic
 
@@ -7,9 +18,17 @@ $(document).ready(function() {
   $("form#new-location").submit(function(event) {
     event.preventDefault();
 
-    var inputtedLocale = $("input#new-place").val();
+    var inputtedLocaleName = $("input#new-place").val();
+    var inputtedLandmark = $("input#new-landmark").val();
+    var inputtedTimeOfYear = $("input#new-toy").val();
+    var inputtedAccomodations = $("input#new-accom").val();
+    var inputtedActivities = $("input#new-activities").val();
+    var inputtedNotes = $("input#new-notes").val();
 
-    $("#reveal-location").append("<li>" + inputtedLocale + "</li>");
+    var newLocation = new Location(inputtedLocaleName, inputtedLandmark, inputtedTimeOfYear, inputtedAccomodations, inputtedActivities, inputtedNotes);
+
+    $("#reveal-location").append("<li>" + newLocation.displayLocation() + "</li>");
+
     $("#reveal-location").show();
 
   });
